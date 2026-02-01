877cf8a71f8c89c026a029101253f51c0f572e8c
Yogesh Anup Shirke <yshirke005@gmail.com>
2026-02-01 19:55:27 +0530

    Initial commit

---
+++ src/main/java/com/example/wearify/HomeController.java
@@ -0,0 +1,43 @@
+package com.example.wearify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/mens")
+    public String mens() {
+        return "mens";
+    }
+
+    @GetMapping("/womens")
+    public String womens() {
+        return "womens";
+    }
+
+    @GetMapping("/accessories")
+    public String accessories() {
+        return "accessories";
+    }
+
+    @GetMapping("/shoes")
+    public String shoes() {
+        return "shoes";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/checkout")
+    public String checkout() {
+        return "checkout";
+    }
+}
